27a30d70866ee4640ae864051a779af5865e8205
Apirak Fakin <apirakfakin@gmail.com>
2024-05-09 14:24:19 +0700

    feat: add ex-8

--- ex-7.js
+++ ex-7.js
@@ -4,5 +4,6 @@ let subject = "I";
 let verb = "am";
 let object = "A Software Developer";
 
-let sentence = subject + " " + verb + " " + object;
+let sentence;
+sentence = subject + " " + verb + " " + object;
 console.log(sentence);
